b95768894d6150aaec26285cd997a17ddb38a46b
inizumi03 <147955660+inizumi03@users.noreply.github.com>
2026-02-19 19:39:36 -0300

    d

--- final-de-verano-moviles/Assets/codigos/ItemManager.cs
+++ final-de-verano-moviles/Assets/codigos/ItemManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject prefabItem;
+    public Transform[] puntosSpawn;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (Transform punto in puntosSpawn)
+        {
+            if (punto != null)
+            {
+                Instantiate(prefabItem, punto.position, Quaternion.identity);
+            }
+        }
     }
 }
